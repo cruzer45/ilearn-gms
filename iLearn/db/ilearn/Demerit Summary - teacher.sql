0eab34d00aead0665674f8b461024e64549d751d
cruzer45 <cruzer45@3595d264-2469-fffa-8d35-14a0b1942285>
2011-09-19 16:08:33 +0000

    -   Fixed a bug in the demerit reports where it was counting the demerits instead of summing them

--- iLearn/db/ilearn/Demerit Summary - teacher.sql
+++ iLearn/db/ilearn/Demerit Summary - teacher.sql
@@ -1,6 +1,7 @@
-SELECT CONCAT_WS(' ',`staFirstName`,`staLastName`) AS 'Staff', CONCAT_WS(' ',`stuFirstName`,`stuLastName`) AS 'Name', `stuClsCode`, COUNT(`demID`) AS 'Count'
+SELECT CONCAT_WS(' ',`staFirstName`,`staLastName`) AS 'Staff', CONCAT_WS(' ',`stuFirstName`,`stuLastName`) AS 'Name', `stuClsCode`, sum(`demerits`) AS 'Demerits'
 FROM `Demerits`
 INNER JOIN `Student` ON `Demerits`.`demStuID` = `Student`.`stuID`
 INNER JOIN `Staff` ON `Staff`.`staCode` = `Demerits`.`demStaCode`
 WHERE `demStatus` = 'Active'
 GROUP BY `demStaCode`,`demStuID`
+order by CONCAT_WS(' ',`staFirstName`,`staLastName`) asc , COUNT(`demID`) desc
