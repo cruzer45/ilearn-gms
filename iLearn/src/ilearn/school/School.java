c7c9f73e41a500845600944f564d3a2eddd5b22f
cruzer45 <cruzer45@3595d264-2469-fffa-8d35-14a0b1942285>
2012-02-01 03:53:22 +0000

    Added the passing mark and principal name to the school information screen.
    Added highlighting for grades that didn't meet the passing mark.

--- iLearn/src/ilearn/school/School.java
+++ iLearn/src/ilearn/school/School.java
@@ -23,16 +23,18 @@ public class School
         ArrayList<Object> school = new ArrayList<Object>();
         try
         {
-            String sql = "SELECT `schlID`, `schName`, `schShortName`, `schPhone1`, `schPhone2`, `schAddress`, `schLogo`, `registrationCode`, `timeCode`, `dbVersion` FROM `School`;";
+            String sql = "SELECT `schlID`, `schName`, `schShortName`, `schPhone1`, `schPhone2`, `schAddress`, `schLogo`, `registrationCode`, `timeCode`, `dbVersion` , `schPassingMark` , `schPrincipal` FROM `School`;";
             PreparedStatement prep = Environment.getConnection().prepareStatement(sql);
             ResultSet rs = prep.executeQuery();
             rs.first();
-            school.add(rs.getString("schName"));
-            school.add(rs.getString("schShortName"));
-            school.add(rs.getString("schPhone1"));
-            school.add(rs.getString("schPhone2"));
-            school.add(rs.getString("schAddress"));
-            school.add(rs.getBlob("schLogo"));
+            school.add(rs.getString("schName"));//0
+            school.add(rs.getString("schShortName"));//1
+            school.add(rs.getString("schPhone1"));//2
+            school.add(rs.getString("schPhone2"));//3
+            school.add(rs.getString("schAddress"));//4
+            school.add(rs.getBlob("schLogo"));//5
+            school.add(rs.getString("schPassingMark"));//6
+            school.add(rs.getString("schPrincipal"));//7
             rs.close();
             prep.close();
         }
@@ -44,13 +46,13 @@ public class School
         return school;
     }
 
-    public static boolean updateSchoolInfo(String schName, String schShortName, String schPhone1, String schPhone2, String schAddress, File schLogo)
+    public static boolean updateSchoolInfo(String schName, String schShortName, String schPhone1, String schPhone2, String schAddress, File schLogo, String schPassingMark, String schPrincipal)
     {
         boolean successful = false;
         try
         {
             FileInputStream fis = new FileInputStream(schLogo);
-            String sql = "UPDATE `School` SET `schName`= ?, `schShortName`=?, `schPhone1`=?, `schPhone2`= ?, `schAddress`= ? , schLogo = ? WHERE `schlID`=1 LIMIT 1;";
+            String sql = "UPDATE `School` SET `schName`= ?, `schShortName`=?, `schPhone1`=?, `schPhone2`= ?, `schAddress`= ? , schLogo = ? , schPassingMark = ? , schPrincipal = ? WHERE `schlID`=1 LIMIT 1;";
             PreparedStatement prep = Environment.getConnection().prepareStatement(sql);
             prep.setString(1, schName);
             prep.setString(2, schShortName);
@@ -58,6 +60,8 @@ public class School
             prep.setString(4, schPhone2);
             prep.setString(5, schAddress);
             prep.setBlob(6, fis, schLogo.length());
+            prep.setString(7, schPassingMark);
+            prep.setString(8, schPrincipal);
             prep.executeUpdate();
             prep.close();
             successful = true;
@@ -70,18 +74,20 @@ public class School
         return successful;
     }
 
-    public static boolean updateSchoolInfo(String schName, String schShortName, String schPhone1, String schPhone2, String schAddress)
+    public static boolean updateSchoolInfo(String schName, String schShortName, String schPhone1, String schPhone2, String schAddress, String schPassingMark, String schPrincipal)
     {
         boolean successful = false;
         try
         {
-            String sql = "UPDATE `School` SET `schName`= ?, `schShortName`=?, `schPhone1`=?, `schPhone2`= ?, `schAddress`= ?  WHERE `schlID`=1 LIMIT 1;";
+            String sql = "UPDATE `School` SET `schName`= ?, `schShortName`=?, `schPhone1`=?, `schPhone2`= ?, `schAddress`= ? , schPassingMark = ? , schPrincipal = ? WHERE `schlID`=1 LIMIT 1;";
             PreparedStatement prep = Environment.getConnection().prepareStatement(sql);
             prep.setString(1, schName);
             prep.setString(2, schShortName);
             prep.setString(3, schPhone1);
             prep.setString(4, schPhone2);
             prep.setString(5, schAddress);
+            prep.setString(6, schPassingMark);
+            prep.setString(7, schPrincipal);
             prep.executeUpdate();
             prep.close();
             successful = true;
@@ -114,4 +120,25 @@ public class School
         }
         return passingmark;
     }
+    public static String getPrincipal()
+    {
+        String principal = "";
+        try
+        {
+            String sql = "SELECT `schPrincipal` FROM `School`;";
+            PreparedStatement prep = Environment.getConnection().prepareStatement(sql);
+            ResultSet rs = prep.executeQuery();
+            while (rs.next())
+            {
+                principal = rs.getString("schPrincipal");
+            }
+        }
+        catch (Exception e)
+        {
+            String message = "An error occurred while retreiving the passing mark.";
+            logger.log(Level.SEVERE, message, e);
+        }
+        return principal;
+    }
+    
 }
