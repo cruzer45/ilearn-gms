5c7191e39d99fda10081ccdfcb96196cf9715fa7
cruzer45 <cruzer45@3595d264-2469-fffa-8d35-14a0b1942285>
2012-02-10 17:26:58 +0000

    Displaying student photo.

---
+++ iLearn-grails/test/unit/ilearn/classes/ClassesTests.groovy
@@ -0,0 +1,17 @@
+package ilearn.classes
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Classes)
+class ClassesTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
