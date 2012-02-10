3d1f1fd8a980c57ab32a85db0435f6a283f11333
cruzer45 <cruzer45@3595d264-2469-fffa-8d35-14a0b1942285>
2012-02-10 15:58:48 +0000

    Initial Import

---
+++ iLearn-grails/grails-app/domain/ilearn/student/Student.groovy
@@ -0,0 +1,33 @@
+package ilearn.student
+
+class Student
+{
+	int id
+	String socialSecurityNumber
+	String firstName
+	String lastName
+	String otherNames
+	Date dateOfBirth
+
+
+	static mapping =
+	{
+		table 'Student'
+		id column:'stuID'
+		socialSecurityNumber column:'stuSSN'
+		firstName column:'stuFirstName'
+		lastName column:'stuLastName'
+		otherNames column:'stuOtherNames'
+		dateOfBirth column:'stuDOB'
+	}
+
+	static constraints =
+	{
+		id( display:true)
+		socialSecurityNumber()
+		firstName()
+		lastName()
+		otherNames()
+		dateOfBirth()
+	}
+}
