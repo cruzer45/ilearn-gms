339f9d1b45054bd73e7b61387d598271ff3d85eb
cruzer45 <cruzer45@3595d264-2469-fffa-8d35-14a0b1942285>
2012-02-14 14:35:48 +0000

    Added the zk library

---
+++ iLearn-grails/grails-app/composers/ilearn/grails/StudentComposer.groovy
@@ -0,0 +1,13 @@
+package ilearn.grails
+
+import org.zkoss.zk.grails.composer.*
+
+import org.zkoss.zk.ui.select.annotation.Wire
+import org.zkoss.zk.ui.select.annotation.Listen
+
+class StudentComposer extends GrailsComposer {
+
+    def afterCompose = { window ->
+        // initialize components here
+    }
+}
