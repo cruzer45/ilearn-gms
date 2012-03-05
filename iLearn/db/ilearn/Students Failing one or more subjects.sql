5c53abf97e984abbdd3cc94de046909e2a63c6cf
cruzer45 <cruzer45@3595d264-2469-fffa-8d35-14a0b1942285>
2012-03-05 00:56:50 +0000

    iLearn version 1.7.6
    Included database settings editor.

--- iLearn/db/ilearn/Students Failing one or more subjects.sql
+++ iLearn/db/ilearn/Students Failing one or more subjects.sql
@@ -1,6 +1,5 @@
 SELECT CONCAT_WS(' ',`stuFirstName`,`stuLastName`) AS 'Name', `Grade`.`graClsCode`,`graSubCode`,`graFinal`
 FROM `Grade`
 INNER JOIN `Student` ON `Student`.`stuID` = `Grade`.`graStuID`
-WHERE `graFinal` < 60 
-
-order by `graClsCode`, `graStuID`, `graSubCode`, CONCAT_WS(' ',`stuFirstName`,`stuLastName`)
+WHERE `graFinal` < 60
+ORDER BY `graClsCode`, `graStuID`, `graSubCode`, CONCAT_WS(' ',`stuFirstName`,`stuLastName`)
