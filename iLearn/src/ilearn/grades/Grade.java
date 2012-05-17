cc718fd3098ca1ba559c1dea941ec2a26b8f5cb5
cruzer45 <cruzer45@3595d264-2469-fffa-8d35-14a0b1942285>
2012-05-17 22:22:14 +0000

    removed unused classes in the grade class

--- iLearn/src/ilearn/grades/Grade.java
+++ iLearn/src/ilearn/grades/Grade.java
@@ -11,8 +11,6 @@ import ilearn.term.Term;
 import ilearn.user.User;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Vector;
 import java.util.concurrent.TimeUnit;
