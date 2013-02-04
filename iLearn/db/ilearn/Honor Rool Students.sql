c4b9e8a7339610371c4a297a5a9b05485320a8df
cruzer45@gmail.com <cruzer45@gmail.com@3595d264-2469-fffa-8d35-14a0b1942285>
2013-02-04 19:12:51 +0000

    Tweaked the Report a bit

--- iLearn/db/ilearn/Honor Rool Students.sql
+++ iLearn/db/ilearn/Honor Rool Students.sql
@@ -1,5 +1,5 @@
-SELECT CONCAT_WS(' ',`stuFirstName`,`stuLastName`) AS 'Name',`graAvgClsCode`, `Grade_Average`.`graAvgFinal`
+SELECT CONCAT_WS(' ',`stuFirstName`,`stuLastName`) AS 'Name',`graAvgClsCode` AS 'Class', `Grade_Average`.`graAvgFinal` AS 'Grade'
 FROM `Grade_Average`
 INNER JOIN `Student` ON `Student`.`stuID` = `Grade_Average`.`graAvgStuID`
-WHERE `graAvgFinal` > 80
-order by `graAvgFinal` desc
+WHERE `graAvgFinal` >= 80 AND graAvgTerm = 3
+ORDER BY `graAvgFinal` DESC
